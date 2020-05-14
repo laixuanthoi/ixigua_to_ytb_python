e27cfcc1001736855a30369813ecf21adb9ce27a
laixuanthoi <laixuanthoi@gmail.com>
2020-05-14 09:22:20 +0700

    okk

--- lib/ixigua_spy/spy.py
+++ lib/ixigua_spy/spy.py
@@ -1,4 +1,7 @@
 import requests
+from bs4 import BeautifulSoup
+from urllib.request import urlretrieve
+import progressbar
 
 class Ixigua_Spyder:
     def __init__(self):
@@ -10,9 +13,12 @@ class Ixigua_Spyder:
         
         self.signature = "ztrOnAAgEAS7K0XC317YTc7azoAAJBp"
         self.videos_user_url = "https://www.ixigua.com/home/{}/video"
-        self.videos_user_api = "https://www.ixigua.com/api/videov2/author/video?_signature={}=&author_id={}&type=video&max_time=0"
+        self.videos_user_api = "https://www.ixigua.com/api/videov2/author/video?_signature={}=&author_id={}&type=video&max_time={}"
         self.verify_cookie_api = "https://pc-basic.ixigua.com/api/verify?_signature={}&key={}&psm=toutiao.fe.xigua_video_web_pc"
-
+        self.thumb_url = "{}~tplv-crop-center:1440:810.jpg"
+        self.video_url = "https://www.ixigua.com/i{}"
+        self.videofk_url = "https://www.videofk.com/index-video-download/search?url={}"
+        self.pbar = None
         self.verify_cookie()
         
     def verify_cookie(self):
@@ -22,11 +28,47 @@ class Ixigua_Spyder:
     
     def getListVideo(self, userId):
         arrVideo = []
-        json = self.session.get(self.videos_user_api.format(self.signature, userId), headers={"referer": self.videos_user_url.format(userId)}).json()
-        print(json)
+        max_time = 0
+        has_more = True
+        while has_more:
+            obj = self.session.get(self.videos_user_api.format(self.signature, userId, max_time), headers={"referer": self.videos_user_url.format(userId)}).json()
+            for item in obj["data"]["data"]:
+                arrVideo.append(item)
+            
+            max_time = arrVideo[-1]["behot_time"]
+            has_more = obj["data"]["has_more"]
+
+        return arrVideo
+
+    def show_download_progress(self, block_num, block_size, total_size):
+        if self.pbar is None:
+            self.pbar = progressbar.ProgressBar(maxval=total_size)
+            self.pbar.start()
+
+        downloaded = block_num * block_size
+        if downloaded < total_size:
+            self.pbar.update(downloaded)
+        else:
+            self.pbar.finish()
+            self.pbar = None
+
+    def downloadVideo(self, videoId):
+        if 'i' in videoId:
+            videoId = videoId.replace('i','')
+        url = self.video_url.format(videoId)
+        api = self.videofk_url.format(url)
+        html = requests.get(api).text
+        soup = BeautifulSoup(html, "html.parser")
+        download_link = soup.find("a", {"class": "download"})['href']
+        videoName = "{}.mp4".format(videoId)
+        print("Download video: {}".format(videoName))
+        urlretrieve(download_link, videoName, self.show_download_progress)
 
 
-userId = "1613708881833779"
+
+# userId = "1613708881833779"
+userId = "98175235867"
 
 spy = Ixigua_Spyder()
-spy.getListVideo(userId)
+# spy.getListVideo(userId)
+spy.downloadVideo("6825810893356401156")
